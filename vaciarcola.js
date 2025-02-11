72f22a581c3324cc904c49d263d102df682b5009
Agustt123 <agustintracheskyoficial@gmail.com>
2025-02-11 10:54:49 -0300

    Update vaciarcola.js

--- vaciarcola.js
+++ vaciarcola.js
@@ -4,30 +4,50 @@ const QUEUE_NAME = 'envioML_from_callback'; // Cambia esto por el nombre de tu c
 const RABBITMQ_URL = 'amqp://lightdata:QQyfVBKRbw6fBb@158.69.131.226:5672'; // Cambia esto por la URL de tu RabbitMQ
 
 const vaciarCola = async () => {
-  try {
-    // Conectar a RabbitMQ
-    const connection = await amqp.connect(RABBITMQ_URL);
-    const channel = await connection.createChannel();
+  let connection;
+  let channel;
 
-    // Asegurarse de que la cola existe
-    await channel.assertQueue(QUEUE_NAME, { durable: false });
+  const reconnect = async () => {
+    try {
+      // Intentar conectarse a RabbitMQ
+      connection = await amqp.connect(RABBITMQ_URL);
+      channel = await connection.createChannel();
 
-    //console.log(`Escuchando mensajes en la cola: ${QUEUE_NAME}`);
+      // Asegurarse de que la cola existe
+      await channel.assertQueue(QUEUE_NAME, { durable: false });
 
-    // Consumir mensajes de la cola
-    channel.consume(QUEUE_NAME, (msg) => {
-      if (msg !== null) {
-      //  console.log(`Mensaje recibido: ${msg.content.toString()}`);
-        // Aquí puedes procesar el mensaje si es necesario
+      // Consumir mensajes de la cola
+      channel.consume(QUEUE_NAME, (msg) => {
+        if (msg !== null) {
+          console.log(`Mensaje recibido: ${msg.content.toString()}`);
+          // Aquí puedes procesar el mensaje si es necesario
 
-        // Acknowledge el mensaje para eliminarlo de la cola
-        channel.ack(msg);
-      }
-    }, { noAck: false });
+          // Acknowledge el mensaje para eliminarlo de la cola
+          channel.ack(msg);
+        }
+      }, { noAck: false });
 
-  } catch (error) {
-    //console.error('Error al conectar a RabbitMQ:', error);
-  }
+      // Manejo de cierre y errores de conexión
+      connection.on('error', (err) => {
+        console.error('Error en la conexión de RabbitMQ:', err);
+        if (err.code === 'ECONNRESET') {
+          console.log('Se ha perdido la conexión, reintentando...');
+          setTimeout(reconnect, 5000); // Reintentar después de 5 segundos
+        }
+      });
+
+      connection.on('close', () => {
+        console.log('Conexión cerrada, reintentando...');
+        setTimeout(reconnect, 5000); // Reintentar después de 5 segundos
+      });
+
+    } catch (error) {
+      console.error('Error al conectar a RabbitMQ:', error);
+      setTimeout(reconnect, 5000); // Reintentar después de 5 segundos
+    }
+  };
+
+  await reconnect();
 };
 
 // Iniciar el proceso
